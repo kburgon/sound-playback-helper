4921da4077bdd6447701d82888ac4b556d7bab6b
Kevin Burgon <buburgo@gmail.com>
2022-09-05 22:05:27 -0600

    Add compatibility for custom sound directory path

--- sound_player.py
+++ sound_player.py
@@ -4,9 +4,9 @@ from os import listdir
 from os.path import isfile, join
 
 class SoundPlayer(object):
-    def __init__(self):
-        self.root_path = 'Sounds/'
-        self.all_sounds = self.list_sounds()
+    def __init__(self, path='Sounds/'):
+        self.root_path = path
+        self.all_sounds = []
         self.current_index = 0
     def list_sounds(self):
         return [f for f in listdir(self.root_path) if isfile(join(self.root_path, f))]
@@ -16,7 +16,9 @@ class SoundPlayer(object):
         song = AudioSegment.from_mp3(full_name)
         play(song)
     def play_rotated_sound(self):
-        sound = self.all_sounds[self.current_index]
+        if self.all_sounds == []:
+            sound = self.all_sounds[self.current_index]
+            self.current_index = 0
         self.play(sound)
         self.current_index += 1
         if self.current_index == len(self.all_sounds):
