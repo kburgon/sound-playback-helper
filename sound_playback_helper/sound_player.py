231526010665039d28a1fad08f175fabeeaffe82
Kevin Burgon <kevin.burgon@gmail.com>
2022-09-15 21:02:06 -0600

    Adjust rotating player moethod logic
    Adjusts the logic such that a custom error is raised when no sound are
    located in the sound directory

--- sound_playback_helper/sound_player.py
+++ sound_playback_helper/sound_player.py
@@ -3,23 +3,28 @@ from pydub.playback import play
 from os import listdir
 from os.path import isfile, join
 
+
 class SoundPlayer(object):
     def __init__(self, path='Sounds/'):
         self.root_path = path
         self.all_sounds = []
         self.current_index = 0
+
     def list_sounds(self):
         return [f for f in listdir(self.root_path) if isfile(join(self.root_path, f))]
+
     def play(self, sound_name):
         print('playing name ' + sound_name)
         full_name = self.root_path + sound_name
         song = AudioSegment.from_mp3(full_name)
         play(song)
+
     def play_rotated_sound(self):
         if self.all_sounds == []:
-            sound = self.all_sounds[self.current_index]
-            self.current_index = 0
-        self.play(sound)
-        self.current_index += 1
+            raise Exception('No sounds found in directory')
         if self.current_index == len(self.all_sounds):
             self.current_index = 0
+        sound = self.all_sounds[self.current_index]
+        self.current_index = 0
+        self.play(sound)
+        self.current_index += 1
