0b5a7e75b853018197d7e420f5013ec3d74eb708
Kevin Burgon <buburgo@gmail.com>
2025-10-24 16:45:19 -0600

    Load sounds up front, properly rotate through sounds

--- sound_playback_helper/sound_player.py
+++ sound_playback_helper/sound_player.py
@@ -8,23 +8,27 @@ class SoundPlayer(object):
     def __init__(self, path='Sounds/'):
         self.root_path = path
         self.current_index = 0
+        self.loaded_sounds = self.load_all()
 
     def list_sounds(self):
         return [f for f in listdir(self.root_path) if isfile(join(self.root_path, f))]
 
-    def play(self, sound_name):
-        print('playing name ' + sound_name)
-        full_name = self.root_path + sound_name
-        song = AudioSegment.from_mp3(full_name)
-        play(song)
+    def load(self, name):
+        fullName = self.root_path + name
+        return AudioSegment.from_mp3(full_name)
+
+    def load_all(self):
+        return [AudioSegment.from_mp3(self.root_path + f) for f in listdir(self.root_path) if isfile(join(self.root_path, f))]
+
+    def play(self, sound):
+        play(sound)
 
     def play_rotated_sound(self):
-        all_sounds = self.list_sounds()
+        all_sounds = self.loaded_sounds
         if all_sounds == []:
             raise Exception('No sounds found in directory ' + self.root_path)
-        if self.current_index == len(all_sounds):
+        if self.current_index >= len(all_sounds):
             self.current_index = 0
         sound = all_sounds[self.current_index]
-        self.current_index = 0
         self.play(sound)
         self.current_index += 1
